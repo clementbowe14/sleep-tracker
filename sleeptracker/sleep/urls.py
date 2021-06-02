773f4776734725a1738b61f15064cc84ddcc63f3
Clement Bowe <cbowe023@fiu.edu>
2021-06-01 22:08:49 -0400

    created urls & views
    fixed serializer imports
    added CRUD urls for sleeps

---
+++ sleeptracker/sleep/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from sleep import views
+
+urlpatterns = [
+    path('sleep/', views.SleepList.as_view())
+    path('sleep/<int:pk>', views.SleepDetail.as_view()),
+]
--- sleeptracker/sleep/views.py
+++ sleeptracker/sleep/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from sleep.models import sleep
 from sleep.serializers import SleepSerializer
 from rest_framework import generics
-from .models import views.py
 # Create your views here.
 
 
 # create new sleep
 class SleepList(generics.ListCreateAPIView):
     queryset = Sleep.objects.all()
-    seriliazer_class Sleepseri
+    seriliazer_class = SleepSerializer
+
+class SleepDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Sleep.objects.all()
+    serializer_class = SleepSerializer
 
