a745375f9866068bf8228db33a130863e6952009
Clement Bowe <cbowe023@fiu.edu>
2021-06-21 15:27:57 -0400

    minor changes to format

--- frontend/src/pages/splashPage.js
+++ frontend/src/pages/splashPage.js
@@ -1,26 +1,27 @@
 import styled, { css } from "styled-components";
-import { Box, Heading, Flex} from "@chakra-ui/react";
+import { Center, Box, Heading, Flex, Button, Container } from "@chakra-ui/react";
 import "./splashpage.css";
 
 export const Background = styled.section`
   margin: 0;
   min-height: 100vh;
-  width: cover;
-  height: cover;
+  width: 100%;
+  height: 100%;
   background: linear-gradient(90deg, #0052d4 0%, #4364f7 50%, #6fb1fc 100%);
 `;
 
 const SplashPage = (props) => {
   return (
-    
-      
-        <Flex align="center">
-            <Box>
-                <Heading size="xl">Dream Notes</Heading>
-            </Box>
-        </Flex>
-      
-    
+  
+    <Center direction="column" w={"100%"} h={"50%"}>
+      <Box className="splash-headings">
+        <Heading>Dream Notes</Heading>
+        <Container id="splash-subheading">
+          
+        </Container>
+      </Box>
+    </Center>
+  
   );
 };
 export default SplashPage;
